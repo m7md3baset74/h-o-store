a097f6bdd2e5f7d6a2547719fb6fb024cf0f6095
m7md3baset74 <mohamedabdelbasset265@gmail.com>
2026-04-06 01:09:47 +0200

    Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
 "use client";
+import { motion } from 'framer-motion';
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
+    <main className="flex flex-col min-h-screen items-center justify-center p-4 sm:p-6 gap-6">
+      <motion.div
+                initial={{ opacity: 0, y: -30 }}
+                animate={{ opacity: 1, y: 0 }}
+                whileHover={{ scale: 1.02 }}
+                transition={{ duration: 0.4 }}
+              >
+              <img
+                  src="/logoIcon.png"
+                  className="w-100 h-100 object-contain bg-gray-900/15 p-1 rounded-lg mx-auto mt-[-8px]"
+                  />
+                  </motion.div>
       <h1 className="text-3xl font-extrabold leading-tight bg-transparent text-transparent
        bg-clip-text bg-gradient-to-r from-blue-800/90 to-green-300 animate-pulse ">H O Store Progress Tracker</h1>
     </main>
